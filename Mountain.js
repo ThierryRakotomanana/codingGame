8e49a843ccf3053c9281b7e660228d7db26d7a92
ThierryRakotomanana <105288252+ThierryRakotomanana@users.noreply.github.com>
2022-05-25 13:35:30 +0300

    Create Mountain.js

---
+++ Mountain.js
@@ -0,0 +1,38 @@
+// game loop
+
+while (true) {
+    let Height = []
+    for (let i = 0; i < 8; i++) {
+        Height[i] = parseInt(readline()); // represents the height of one mountain.
+        
+    }
+    /*
+    Let's copy the array Height
+    */
+    let Maximum = [] ;
+    let max = 0;
+    for (i = 0; i < Height.length; i++) {
+        Maximum[i] = Height[i];
+      }
+    /* 
+    Find the maximu value
+    */
+    for (let i = 0; i < 7; i++) {
+        if(Height[i] < Height[i+1]) {
+            max = Height[i+1]
+        } else {
+            max = Height[i];
+            Height[i] = Height[i+1];
+            Height[i+1] = max
+        }
+        
+    }
+    
+    console.log(Maximum.indexOf(Height[7]));
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+        // The index of the mountain to fire on.
+
+}
