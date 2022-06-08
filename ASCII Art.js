5198e662de75d0366ab6c5fe990e627aa33168f7
ThierryRakotomanana <105288252+ThierryRakotomanana@users.noreply.github.com>
2022-06-08 17:00:02 +0300

    Update ASCII Art.js

--- ASCII Art.js
+++ ASCII Art.js
@@ -1,1 +1,35 @@
 
+let T = 'Sal*'
+const MAJ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let Texte = T.toUpperCase().split('')
+let affiche = ''
+
+let Row = []
+Row[0]  = ' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### '
+Row[1]  = '# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # '
+Row[2]  = '### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## '
+Row[3]  = '# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       '
+Row[4]  = '# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  '
+let texte = []
+let Asc = ''
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < Texte.length; j++) {
+      if( MAJ.indexOf(Texte[j]) === -1){ 
+        for (let k = 4 * parseInt(MAJ.length); k <  4 * parseInt(MAJ.length) + 4 ; k++) {
+             Asc += Row[i][k]
+          }
+        affiche += Asc
+        Asc = ''
+      }else{
+          for (let k = 4 * parseInt(MAJ.indexOf(Texte[j])); k <  4 * parseInt(MAJ.indexOf(Texte[j])) + 4 ; k++) {
+             Asc += Row[i][k]
+          }
+          affiche += Asc
+          Asc = ''
+      } 
+  }
+  texte[i] = affiche
+  affiche = ''
+  console.log(texte[i])
+}
