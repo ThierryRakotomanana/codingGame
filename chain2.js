a454a7970da061c54ed65529d264f4a55c66e6e3
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-07-21 16:38:56 +0300

    Update chain2.js
    it doesn't work

--- chain2.js
+++ chain2.js
@@ -1,7 +1,9 @@
 
-let words = [] 
-words[1] = ["a","b","ba","bca","bda","bdca", 'c' , 'e','cd','cde','fcde','ce','fcdek','bdkca']
+let words = ["a","b","ba","bca","bda","bdca", 'c' , 'e','efg','efgh','efghi','efghij','efghijk','efghijkl'] 
+words[0] = ["a","b","ba","bca","bda","bdca", 'c' , 'e','cd','cde','fcde','ce','fcdek','bdkca']
 words[0] = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+words[1] = ["ksqvsyq","ks","kss","czvh","zczpzvdhx","zczpzvh","zczpzvhx","zcpzvh","zczvh","gr","grukmj","ksqvsq","gruj","kssq","ksqsq","grukkmj","grukj","zczpzfvdhx","gru"]
+
 
 // return an array of length
 function length(arr){
@@ -51,13 +53,23 @@ function listOfIndexOfString(arr, length){
   return values
 }
 
+function searchIndex(value, arr){
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if(value == arr[i]){
+            console.log(i)
+            return i
+        }
+    }
+    return -1
+}
+/*
 //verify if all values of arr is in inArr
 function indexOfString(arr , inArr){
   let isInArray = false
   let compteur = 0
   if(arr.length == inArr.length - 1 && 1 < arr.length ){
     for(let i = 0; i < arr.length -1; i++){
-      if( inArr.indexOf(arr[i+1]) != -1 && inArr.indexOf(arr[i]) != -1 && inArr.indexOf(arr[i]) < inArr.indexOf(arr[i+1])){
+      if( inArr.indexOf(arr[i]) != -1 && inArr.indexOf(arr[i]) < searchIndex(arr[i+1], inArr)){
         ++compteur
       }
     }
@@ -67,7 +79,22 @@ function indexOfString(arr , inArr){
   }
   return isInArray
 }
-
+*/
+function indexOfString(arr , inArr){
+  let isInArray = false
+  let compteur = 0
+  if(arr.length == inArr.length - 1 && 1 < arr.length ){
+    for(let i = 0; i < arr.length -1; i++){
+      if( inArr.indexOf(arr[i]) != -1 && (searchIndex(arr[i], inArr) < searchIndex(arr[i+1], inArr))){
+        ++compteur
+      }
+    }
+    if(compteur == arr.length - 1) isInArray = true
+  } else if(arr.length == 1 && inArr.indexOf(arr[0]) != -1){
+    isInArray = true
+  }
+  return isInArray
+}
 
 
 function Clone(arr){
@@ -105,7 +132,7 @@ function compareArray(first, second, p , chain, position, longChain){
         position = []
     }
 }
-
+/*
 let Length = length(words[0])
 let del = ordinary(deleteDoublon(Length))
 let chain = [], longChain = [], position = []
@@ -113,5 +140,6 @@ for(let i = 0; i < del.length ; i++){
   chain[i] = listOfIndexOfString(words[0], del[i])
 }
 compareArray(chain[0], chain[1], 1, chain, position, longChain)
+*/
 
-console.log(longChain)
+console.log(indexOfString("zczpzvh","zczpzvhx"))
