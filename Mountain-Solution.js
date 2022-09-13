9524c4fa697eb8d32b890324f64a036bd9405bf8
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-09-13 15:41:07 +0300

    docs: describe problem

---
+++ Mountain-Solution.js
@@ -0,0 +1,54 @@
+
+/*
+
+Try to find the index of the mountain who have the height more big
+*/
+// game loop
+
+while (true) {
+    let Height = []
+    for (let i = 0; i < 8; i++) {
+        Height[i] = parseInt(readline()); // represents the height of one mountain.
+        
+    }
+    /*
+    Let's copy the array Height
+    */
+    let Maximum = [] ;
+    let max = 0;
+    for (i = 0; i < Height.length; i++) {
+        Maximum[i] = Height[i];
+      }
+    /* 
+    Find the maximu value
+    */
+    for (let i = 0; i < 7; i++) {
+        if(Height[i] < Height[i+1]) {
+            max = Height[i+1]
+        } else {
+            max = Height[i];
+            Height[i] = Height[i+1];
+            Height[i+1] = max
+        }
+        
+    }
+    
+    console.log(Maximum.indexOf(Height[7]));
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+        // The index of the mountain to fire on.
+
+}
+//another way more simple
+
+while (true) {
+    let Height = []
+    for (let i = 0; i < 8; i++){
+        Height[i] = parseInt(readline()); // represents the height of one mountain.    
+    }
+
+    Height.forEach(( item, i, array ) = { if( max < item)  max = item });
+    console.log(Height.indexOf(max)); // The index of the mountain to fire on.
+}
---
+++ Unary-Problem.md
@@ -0,0 +1,14 @@
+## SOLVE UNARY PROBLEM
+ >link is bellow
+[UNARY](https://www.codingame.com/training/easy/unary)
+
+## External ressource usefull
+Try to see this concept before to solve problem
+ ''[Masquage](https://fr.wikipedia.org/wiki/Masquage) , [Décalage de bits](https://fr.wikipedia.org/wiki/Op%C3%A9ration_bit_%C3%A0_bit#D.C3.A9calages_de_bit) , [Table ASCII](http://ascii.cl/)
+
+## What you'll learn
+You will leanr this concept
+ ''Chaînes de caractères, Encodage, Boucles, Conditions
+
+## Solution
+[Unary.js](./Unary.js)
