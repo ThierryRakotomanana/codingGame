28be0e52a945e93583e515442977d2363908c012
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-10-05 17:30:26 +0300

    chore: add solution in progress

--- Shadows of the Knight - Episode 1.js
+++ Shadows of the Knight - Episode 1.js
@@ -10,6 +10,7 @@ const N = parseInt(readline()); // maximum number of turns before game over.
 var inputs = readline().split(' ');
 const X0 = parseInt(inputs[0]);
 const Y0 = parseInt(inputs[1]);
+let only = true, X = 0, Y = 0, position = 0, position1 = 0 ;
 
 // game loop
 while (true) {
@@ -17,8 +18,38 @@ while (true) {
 
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
+    if(only === true){
+        X = H - (X0 + 1) 
+        Y = W - (Y0 + 1) 
+        position0 = X0 
+        position1 = Y0 
+    }else{
+        X = X
+        Y = Y
+    }
+    only == true ? : X = X, Y = Y
 
-
+    if(bombDir == 'U'){
+        position0 = position0 + Math.round(X/2)
+        X = Math.round(X/2)
+        position1 = position1 + Math.round(Y/2)
+        Y = Math.round(Y/2)
+        console.log('0 0');
+    }else if(bombDir == "UR"){
+        console.log('0 0');
+    }else if(bombDir == "R"){
+        console.log('0 0');
+    }else if(bombDir == "DR"){
+        console.log('0 0');
+    }else if(bombDir == "D"){
+        console.log('0 0');
+    }else if(bombDir == "DL"){
+        console.log('0 0');
+    }else if(bombDir == "L"){
+        console.log('0 0');
+    }else {
+        console.log('0 0');
+    }
+    only = false
     // the location of the next window Batman should jump to.
-    console.log('0 0');
 }
